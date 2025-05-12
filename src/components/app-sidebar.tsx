5736df4e0996cb02200ec8bb00d58f1388eab9da
ioanefaumui <johnfaumui@gmail.com>
2025-05-12 19:13:00 -0300

    feat: add shadcn sidebar

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,46 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./ui/sidebar";
+
+import { Boxes, Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+
+const items = [
+  {
+    title: "Produtos",
+    url: "#",
+    icon: Boxes,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
