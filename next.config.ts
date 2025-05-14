6d66862b2f736e10d3e5fee7e047f1249f14f08f
ioanefaumui <johnfaumui@gmail.com>
2025-05-14 07:47:04 -0300

    chore: config img remote source

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/products",
+        destination: "http://localhost:4000/products",
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "/id/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
